9083796ed7bc5a986869998f3dd32c296cb372a1
chtse <chtse@iutclinfa1901l.iut.local>
2018-10-26 13:05:27 +0200

    g

---
+++ planning.php
@@ -0,0 +1,49 @@
+
+<?php
+include('partials/header.php');
+?>
+
+	
+	<div class="content">
+	<img width='100%' src="banner.png">
+		<section>
+			<h2>Planning</h2>
+			<article>
+				<div class="center" >
+			<table class="tabletarif">
+				<tr>
+					<th></th>
+					<th>Lundi</th>
+					<th>Mardi</th>
+					<th>Mercredi</th>
+					<th>Jeudi</th>
+					<th>Vendredi</th>
+					<th>Samedi</th>
+				</tr>
+				<tr>
+					<td>Débutant</td>
+					<td colspan="3">18h-19h</td>
+					<td colspan="3">/</td>
+
+
+				</tr>
+				<tr>
+					<td>Intermédiaire</td>
+					<td colspan="3">/</td>
+					<td colspan="3">18h-19h</td>
+				</tr>
+				<tr>
+					<td>Avancé</td>
+					<td colspan="3">19h-21h</td>
+					<td colspan="3">/</td>
+				</tr>
+			</table>
+		</div>
+			</article>
+		</section>
+
+		
+	</div>
+<?php
+include('partials/footer.php');
+?>
--- presentation.php
+++ presentation.php
@@ -1,16 +1,110 @@
 <?php
 include('partials/header.php');
 ?>
-	<header class="center">
-			<h1 align="center"><a href="index.html">Fédération Magique Française de Quidditch</a></h1>
-	<img width="100%" src="imgtarifs.jpg">
-	</header>
+<header class="masthead">
 
-	<div class="content">
-		<section>
-			<h2>Presentation</h2>
-		</section>
-	</div>
+</header>
+
+<div class="content presentation">
+	<section>
+		<h2>Presentation</h2>
+		<p>Le quidditch est le seul sport collectif obligatoirement mixte (avec un maximum de quatre personnes du même genre
+			par équipe sur le terrain) qui se joue à 7 par équipe et qui se pratique avec quatre balles sur le terrain. Il n’y a
+			pas de mi-temps mais des remplacements peuvent se faire à n’importe quel moment pendant le match, en nombre
+			illimité.
+			Pour résumer, une équipe est composée de :
+
+			3 poursuiveurs/poursuiveuses (qui jouent avec le souafle),
+			2 batteurs/batteuses (qui jouent avec les cognards),
+			1 gardien/gardienne (qui joue avec le souafle)
+			1 attrapeur/ attrapeuse (qui doit attraper le vif-d‘Or/le « snitch » en anglais).</p>
+
+		<p>Le détail des différents postes
+
+			Les poursuiveurs/poursuiveuses (bandeau blanc)
+			Fonction: Marquer le maximum de buts avec le souafle dans l’un des 3 anneaux adverses (10 pts chacun) en évitant les
+			cognards et en évitant de se faire plaquer par les adversaires.</p>
+
+		<p>Les batteurs/batteuses (bandeau noir)
+			Fonction: Toucher les adversaires (avec ou sans balle) le plus souvent possible en lançant le cognard.</p>
+
+		<p>A chaque fois qu’un joueur/une joueuse est touché-e par un cognard, il/elle doit impérativement lâcher la balle
+			(s’il-elle l’a en sa possession), descendre de son balai (afin que tout le monde puisse voir qu’il/elle est hors
+			jeu), et doit ensuite retourner toucher ses buts le plus rapidement possible avant de pouvoir reprendre le jeu.</p>
+
+		<p>Le gardien/la gardienne (bandeau vert)
+			Fonction: Protéger ses buts et mener le jeu de l’équipe. Le gardien/la gardienne peut sortir de sa zone (dans
+			laquelle aucun cognard ne le/la met hors-jeu, et aucun-e joueur-se ne peut le/la plaquer) et aider ses
+			coéquipier-e-s poursuiveurs/poursuiveuses en marquant des buts.</p>
+
+		<p>L’attrapeur/attrapeuse (bandeau jaune)
+			Les attrapeurs/attrapeuses arrivent sur le terrain à la 18ème minute et ont pour mission d’attraper le vif d’or. Il
+			s’agit d’une balle attachée au short d’un-e arbitre neutre habillé-e en jaune (le Vif d’Or). Le but est de
+			l’attraper avant l’équipe adverse (30 pts). Les attrapeurs/attrapeuses n’ont pas le droit d’agripper le Vif-d’Or par
+			le bras ou les vêtements et ne peuvent pas le plaquer.</p>
+	</section>
+	<header class="masthead1">
+
+	</header>
+	<section>
+		<p>Seul-e le/la vif-d’or est autorisé-e à user de contacts physiques pour compliquer la tâche des
+			attrapeurs/attrapeuses ! Dès que le/la vif-d’or est attrapé-e, le match est terminé.</p>
+
+		<p>Le porteur/ la porteuse du vif-d’or (tenue intégrale jaune – sans bandeau ni balai – il/elle porte une chaussette
+			dans laquelle il y a une balle de tennis)
+			arbitre, son but est de contrer les attaques (pour capturer le vif d’or) des deux attrapeurs/attrapeuses afin de
+			faire durer le match.</p>
+
+
+		<p>Pour faire un match
+
+			Le nombre de balles
+			1 souafle pour les poursuiveurs/poursuiveuses,
+			3 cognards pour les batteurs/batteuses,
+			1 vif d’or (chaussette contenant une balle de tennis) pour le/la vif-d’or.</p>
+
+		<p>Le départ, ou brooms up
+			Les joueurs et joueuses sont alignés sur la ligne de départ, située à 3 mètres des buts de son équipe. Les balles
+			sont positionnées au milieu du terrain : le souafle et un cognard à 1m de chaque côté du point central, les cognards
+			restants à 7m. L’arbitre vérifie que tout le monde est prêt en s’adressant d’abord aux arbitres, puis à chaque
+			équipe. Il/elle dit ensuite balai à terre, ce qui signifie que l’on doit se positionner à côté de son balai, genou à
+			terre. Puis « prêt » (on doit conserver une main sur la balai mais on peut lever les deux genoux) et enfin « brooms
+			up » qui signifie le début de la rencontre.</p>
+
+
+
+		<p>Un match n’a pas de durée limite préétablie, il se termine seulement lorsqu’un attrapeur/une attrapeuse arrive à
+			récupérer le vif d’or (30 pts supplémentaires).
+			La majorité des matchs se terminent au bout de 25-30 minutes de jeu, mais parfois un match peut se terminer dès que
+			les attrapeurs/attrapeuses rentrent sur le terrain, soit au bout de 18 minutes (ce qui est frustrant). Tout dépendra
+			des compétences physiques des attrapeurs/attrapeuses et du vif-d’or. Plus ils/elles seront athlétiques, plus le
+			match sera long !</p>
+
+
+
+		<p>La capture du vif d’or ne garantit pas la victoire de l’équipe.
+			Une fois le vif d’or capturé, on ajoute les 30pts de capture à l’équipe, ce qui détermine le résultat final à partir
+			duquel on peut déclarer la fin du match (s’il y a égalité, il existe des prolongations).
+			C’est donc au protecteur/à la protectrice du vif d’or de faire durer le match et aux attrapeurs/attrapeuses de jouer
+			intelligemment, c’est-à-dire, se tenir au courant du score actuel, ne pas se précipiter, et faire en sorte
+			qu’attraper le vif-d’or fasse réellement gagner son équipe. Tâche délicate, car cela peut se jouer à 10 points
+			d’écart.</p>
+
+
+
+		<p>Tout au long du match les joueurs et joueuses ont obligation de garder un balai ou assimilé (bâton, crosse de
+			Hockey
+			etc.) en position, c’est-à-dire, entre les jambes. Peu importe s’il est tenu d’une main ou non, le principal est
+			qu’il ne doit absolument pas tomber par terre ou quitter sa position (entre les jambes) sous peine de devenir
+			hors-jeu.
+		</p>
+
+		<p>La FQF est la Fédération du Quidditch Français. Elle a pour but le développement du Quidditch en France, et la
+			représentation des équipes françaises à l’étranger, notamment auprès des autres fédérations d’Europe et de l’IQA,
+			l’International Quidditch Association.
+		</p>
+	</section>
+</div>
 <?php
 include('partials/header.php');
 ?>
